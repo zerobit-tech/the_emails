675f7390776148c9fd8db5011e7ac55babb77804
sumit <sumit@localhost.localdomain>
2022-11-11 20:32:43 -0600

    faust

--- the_emails/apps.py
+++ the_emails/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EmailsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'the_emails'
+
+    def ready(self) -> None:
+        import the_emails.agents
